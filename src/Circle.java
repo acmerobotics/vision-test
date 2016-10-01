46e33e9a6e309a855190b3f9b91352fe4d1cffd1
Ryan Brott <rcbrott@gmail.com>
2016-10-01 00:27:03 -0700

    Add preliminary button detection code

---
+++ src/Circle.java
@@ -0,0 +1,21 @@
+import org.opencv.core.Point;
+
+public class Circle {
+
+    public Point pt;
+    public int radius;
+
+    public Circle(Point pt, int radius) {
+        this.pt = pt;
+        this.radius = radius;
+    }
+
+    public static Circle fromDoubleArray(double[] arr) {
+        Point pt = new Point();
+        pt.x = Math.round(arr[0]);
+        pt.y = Math.round(arr[1]);
+        int radius = (int) Math.round(arr[2]);
+        return new Circle(pt, radius);
+    }
+
+}
